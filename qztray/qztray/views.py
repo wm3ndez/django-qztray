da8a0f0a0aa8b76ba6c8d8fb8d0dcb3ef5c80350
Williams Mendez <wmendez27@gmail.com>
2015-07-17 09:59:53 -0400

    Bug fixes

--- qztray/qztray/views.py
+++ qztray/qztray/views.py
@@ -1,22 +1,19 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+import base64
+from django.conf import settings
+from M2Crypto import EVP
 
 
 @csrf_exempt
 def sign_printer_request(request):
-    import base64
-    from django.conf import settings
-    from M2Crypto import EVP
-
-    message = request.POST.get('data')
-
     # Load the private key
     # http://chandlerproject.org/Projects/MeTooCrypto
     key = EVP.load_key_string(open(settings.QZTRAY_PRIVATE_KEY).read())
 
     # Create the signature
     key.sign_init()
-    key.sign_update(message)
+    key.sign_update(request.body)
     signature = key.sign_final()
 
     return HttpResponse(base64.b64encode(signature), content_type='text/plain')
